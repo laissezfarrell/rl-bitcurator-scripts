304cf72dd08c9c258dd02643ee791e8518b006c8
Farrell <matthew.j.farrell@duke.edu>
2018-12-03 15:22:09 -0500

    cleaned up dir

--- batchmkdir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Very basic shell script to run brunnhilde.py over every subdirectory in a directory, without recursing.
-
-for D in *; do
-  mkdir ../administrative_metadata/$D
-done
--- sha1batch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Very basic shell script to run sha1deep over every subdirectory in a directory recursing and writing an output file at the directory level.
-
-for D in *; do
-  sha1deep -e -r -W ../administrative_metadata/"$D"_objects.sha1 $D 
-done
