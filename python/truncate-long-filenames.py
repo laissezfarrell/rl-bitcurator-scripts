b26da358d794b452577abed42b0dc308500999c2
farrell <matthew.j.farrell@duke.edu>
2018-12-10 17:22:09 -0500

    uploaded work in progress and errors output.

--- python/truncate-long-filenames.py
+++ python/truncate-long-filenames.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 #Script (in progress) to analyze and truncate filenames in long paths before transferring to NTFS volumes.
 
-#Dependencies: argparse, pathlib
+#Dependencies: argparse, pathlib, python3.6 or above, csv
 
 #Assumptions:
 # 1. Destination is an NTFS volume with a 255-character cap on paths
@@ -10,14 +10,55 @@
 # 3. That the total path length calculated here will not be 100% accurate post-transfer (e.g., windows Path stuff; additional directories on destination, etc.)
 
 from pathlib import Path, PurePath
-import argparse
+import argparse, csv
 
-#Functions
 
-#calculate total path length
+# get all files in a directory and Identify the paths of problematic length
+def scanPath(inputDir): #'inputDir' here being the variable created in selectiveTruncator below
+    startingPath = Path(inputDir)
+    for i in startingPath.rglob('*'):
+        pathObj = i
+        #print(pathObj) #Partial Sanity check. Comment this line when before putting into production.
+        numChar = len(str(pathObj))
+        #print(pathObj,numChar) #Partial sanity check. Comment this line before putting into production.
+        if numChar <= 100: #change this number as needed (lower numbers for testing). Probably ~230 for NTFS.
+            pass
+        else:
+            largeNum = numChar
+            print("The full path length of " + pathObj.name+ " is ", largeNum) #Sanity check?
+            renamer(pathObj)
 
-#determine which part of the path should be edited
+#rename file
+def renamer(pathObj):
+    print(pathObj.stem)
+    counter = 0
+    while True:
+        counter += 1
+        newPath = pathObj.replace(pathObj.with_name('RLRENAMED''{0:03}'.format(counter)))
+        #newPath = Path(pathObj.parent).joinpath("rlRenamed".format(counter), pathObj.suffix)
+        #if not newPath.exists():
+            #return newPath
 
-#truncate/rename file
+#write the CSV
 
-#Main section
+#Main Function
+def scanTriageTrunc():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--directory", required=False, help="Path to input directory. If unused, input will be current directory.")
+    parser.add_argument("-o","--output", required=True, help="Path to and filename for output file. Required")
+    args = parser.parse_args()
+
+    if args.directory:
+        inputDir = args.directory
+    else:
+        inputDir = Path.cwd()
+    if args.output:
+        outputFile = args.output
+
+    #with open (outputFile, 'wb') as f:
+        #writer = csv.writer(f)
+        #writer.writerow(['path', 'origFname', 'newFname'])
+
+    scanPath(inputDir)
+
+scanTriageTrunc()
