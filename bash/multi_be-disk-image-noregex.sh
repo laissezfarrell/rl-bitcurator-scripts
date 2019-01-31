fe5fb8800a957b88189a90d09da5ce500e8d95a1
farrell <matthew.j.farrell@duke.edu>
2019-01-31 09:04:08 -0500

    modified the bulk_extractor bash scripts

---
+++ bash/multi_be-disk-image-noregex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Very basic shell script to run bulk_extractor over every file in a directory,
+# ignoring any subdirectories.
+
+unset a i
+while IFS= read -r -u3 -d $'\0' file; do
+    bulk_extractor -S ssn_mode=1 -S jpeg_carve_mode=0 -S unzip_carve_mode=0 -S unrar_carve_mode=0 -S winpe_carve_mode=0 -e facebook -e outlook -o "$file"_beout $file
+done 3< <(find $@ -maxdepth 1 -type f -print0)
+
---
+++ bash/multi_be-logical-noregex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Very basic shell script to run bulk_extractor over every folder in a directory,
+# ignoring any subdirectories.
+
+unset a i
+for dir in *; do
+    bulk_extractor -S ssn_mode=1 -S jpeg_carve_mode=0 -S unzip_carve_mode=0 -S unrar_carve_mode=0 -S winpe_carve_mode=0 -e facebook -e outlook -o ~/Desktop/be-out/"$dir"_beout -R $dir
+done
