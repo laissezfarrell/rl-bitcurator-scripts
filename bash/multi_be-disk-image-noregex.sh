46e64726f5a86f1c0b26c2f4f5f379727360443d
Farrell <matthew.j.farrell@duke.edu>
2019-05-07 10:19:49 -0400

    created new brunnhilde script and updated two bulk_extractor scripts

--- bash/multi_be-disk-image-noregex.sh
+++ bash/multi_be-disk-image-noregex.sh
@@ -5,6 +5,5 @@
 
 unset a i
 while IFS= read -r -u3 -d $'\0' file; do
-    bulk_extractor -S ssn_mode=1 -S jpeg_carve_mode=0 -S unzip_carve_mode=0 -S unrar_carve_mode=0 -S winpe_carve_mode=0 -e facebook -e outlook -o "$file"_beout $file
+    bulk_extractor -S ssn_mode=1 -S jpeg_carve_mode=0 -S unzip_carve_mode=0 -S unrar_carve_mode=0 -S winpe_carve_mode=0 -e facebook -e outlook -o ~/Desktop/be-out/"$file"_beout/ $file
 done 3< <(find $@ -maxdepth 1 -type f -print0)
-
