49cf257d39e394090fbe24c36cae12ad7d359575
farrell <matthew.j.farrell@duke.edu>
2019-05-22 11:49:59 -0400

    modified accession-reporter.py to create reports in CSV format as well as allow for the option to report in different units.

--- python/accession-reporter.py
+++ python/accession-reporter.py
@@ -11,63 +11,74 @@ import argparse, csv, datetime
 from pathlib import Path, PurePath
 from datetime import datetime
 
-# Function 1: Walk a file tree, identifying the folders therein
 def walkPath():
-    #startingPath = Path(inputDir) #uncomment when ready for arguments
-    startingPath = Path('/home/bcadmin/Desktop/test-data/objects') #comment when ready for arguments
+    startingPath = Path(inputDir) #uncomment when ready for arguments
+    #csvOut = Path('/home/bcadmin/Desktop/accession-report-test.csv')
+    #startingPath = Path('/home/bcadmin/Desktop/test-data/objects') #comment when ready for arguments
     spChild = [x for x in startingPath.iterdir() if x.is_dir()] #create a list of the children directories in startingPath.
-    for i in spChild:
-        operatingDirectory = Path(i)
-        print("the next directory to process is ",operatingDirectory)#sanity check
-        #fileList = list(operatingDirectory.glob('**/*'))
-        fileList = [x for x in operatingDirectory.iterdir() if x.is_file()]
-        print(fileList) #sanity check
-        folderSize = 0
-        fModTime = datetime.now()
-        oldestTime = fModTime
-        newestTime = datetime.strptime("Jan 01 1950", "%b %d %Y")
-        for f in fileList:
-            fSizeBytes = Path.stat(f).st_size
-            folderSize = folderSize + fSizeBytes
-            fModTime = datetime.fromtimestamp(Path.stat(f).st_mtime)
-            if fModTime >= oldestTime:
-                pass
-            else:
-                oldestTime = fModTime
-            if fModTime <= newestTime:
-                pass
-            else:
-                newestTime = fModTime
-        print(folderSize)
-        print(oldestTime)
-        print(newestTime)
-        with open (csvOut, 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(['path','foldersize','Earliest Timestamp','Latest Timestamp'])
+    with open (csvOut, 'w') as m:
+        writer = csv.writer(m)
+        writer.writerow(['path','foldersize '+ labelUnits,'Earliest Timestamp','Latest Timestamp'])
+        for i in spChild:
+            operatingDirectory = Path(i)
+            print("the next directory to process is ",operatingDirectory)#sanity check
+            fileList = list(operatingDirectory.glob('**/*'))
+            #fileList = [x for x in operatingDirectory.iterdir() if x.is_file()]
+            #print(fileList) #sanity check
+            folderSize = 0
+            fModTime = datetime.now()
+            oldestTime = fModTime
+            newestTime = datetime.strptime("Jan 01 1950", "%b %d %Y")
+            for f in fileList:
+                fSizeBytes = (Path.stat(f).st_size / repUnits)
+                folderSize = folderSize + fSizeBytes
+                fModTime = datetime.fromtimestamp(Path.stat(f).st_mtime)
+                if fModTime >= oldestTime:
+                    pass
+                else:
+                    oldestTime = fModTime
+                    if fModTime <= newestTime:
+                        pass
+                    else:
+                        newestTime = fModTime
+            #print(folderSize)
+            #print(oldestTime)
+            #print(newestTime)
             writer.writerow([i,folderSize,oldestTime,newestTime])
+
 
 #end of day May 15: above function calculates the size of the files in a folder, as well as the most recent and oldest date modified. Next steps: 1) add arguments back in and test function. Mostly compied/pasted writer stuff from another script, so potentially doesn't work yet.
 
-
-
-#realTime = datetime.datetime.fromtimestamp(Path.stat(i).st_mtime)
-#print(realTime)
-#print(Path.stat(i).st_size)
-# Function 2: Summarize basic information about the contents of the folders identified in Function 1, including aggregate size, number of files (?), and modified date range.
-
-# Function 3: Create a CSV file including the outputs of FUNCTION 1 and FUNCTION 2
-
 # Main body to accept arguments and call the three functions.
-#parser = argparse.ArgumentParser()
-#parser.add_argument("-o","--output", help="Path to and filename for the CSV to create.")
-#parser.add_argument("-d","--directory", help="Path to input directory.")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("output", help="Path to and filename for the CSV to create.")
+parser.add_argument("input", help="Path to input directory.")
+parser.add_argument("-u", "--units", type=str, choices=["b", "kb", "mb", "gb"], help="Unit of measurement for reporting aggregate size")
+args = parser.parse_args()
 
-#if args.output:
-    #csvOut = args.output
-#if args.directory:
-    #inputDir = args.directory
-
-# function calls go here
+if args.output:
+    csvOut = args.output
+if args.input:
+    inputDir = args.input
+if args.units == "kb":
+    repUnits = 1024
+    labelUnits = "(kilobytes)"
+    print("Reporting sizes in kilobytes")
+elif args.units == "mb":
+    repUnits = 1024*1024
+    labelUnits = "(megabytes)"
+    print("Reporting sizes in megabytes")
+elif args.units =="gb":
+    repUnits = 1024*1024*1024
+    labelUnits = "(gigabytes)"
+    print("Reporting sizes in gigabytes")
+elif args.units == "b":
+    repUnits = 1
+    labelUnits = "(bytes)"
+    print("Reporting sizes in bytes, the purest way to report sizes.")
+else:
+    repUnits = 1
+    labelUnits = "(bytes)"
+    print("Your inattentiveness leads Self to default to reporting size in bytes, the purest yet least human readable way to report sizes. Ha ha, puny human. Bow before Self, the mighty computer. 01000100 01000101 01010011 01010100 01010010 01001111 01011001 00100000 01000001 01001100 01001100 00100000 01001000 01010101 01001101 01000001 01001110 01010011 00101110")
 
 walkPath()
