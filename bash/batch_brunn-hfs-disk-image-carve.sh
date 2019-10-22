60f76a6bcd56fd8d9be2accf22e6a29c42ba0c58
Farrell <matthew.j.farrell@duke.edu>
2019-10-22 14:02:52 -0400

    added two brunnhilde variations

---
+++ bash/batch_brunn-hfs-disk-image-carve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Very basic shell script to run brunnhilde.py over every subdirectory in a directory, without recursing.
+
+#unset a i
+#while IFS= read -r -u3 -d $'\0' d; do
+#    brunnhilde.py --hash sha1 -l ./objects/$d ~/Desktop/brunn-out "$d"
+#done 3< <(find $@ -maxdepth 1 -type f -print0)
+
+for file in *; do
+  brunnhilde.py --hash sha1 -l -d --hfs --resforks "$file" ~/Desktop/brunn-out ${file// /}"_brunnout"
+done
