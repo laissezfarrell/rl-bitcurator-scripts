b68d2056f19377cbdc5f4e9b16583d9f49b74a63
farrell <bobisphat.soisfarrell@gmail.com>
2020-07-10 16:42:35 -0400

    fixed this script a bit

--- bash/grepmail_annual.sh
+++ bash/grepmail_annual.sh
@@ -3,7 +3,10 @@
 # Very basic shell script to grepmail an MBOX file or set of MBOX files into derivative MBOX files per year. Using the date the message was sent rather than received.
 
 for F in *; do
-  grepmail -d "before 2010" "$F" > ~/Desktop/grepmail-out/${F// /}"_before2010.mbox"
+  grepmail -d "before 1/1/2007" "$F" > ~/Desktop/grepmail-out/${F// /}"_before2007.mbox"
+  grepmail -d 2007 "$F" > ~/Desktop/grepmail-out/${F// /}"_2007.mbox"
+  grepmail -d 2008 "$F" > ~/Desktop/grepmail-out/${F// /}"_2008.mbox"
+  grepmail -d 2009 "$F" > ~/Desktop/grepmail-out/${F// /}"_2009.mbox"
   grepmail -d 2010 "$F" > ~/Desktop/grepmail-out/${F// /}"_2010.mbox"
   grepmail -d 2011 "$F" > ~/Desktop/grepmail-out/${F// /}"_2011.mbox"
   grepmail -d 2012 "$F" > ~/Desktop/grepmail-out/${F// /}"_2012.mbox"
@@ -11,8 +14,8 @@ for F in *; do
   grepmail -d 2014 "$F" > ~/Desktop/grepmail-out/${F// /}"_2014.mbox"
   grepmail -d 2015 "$F" > ~/Desktop/grepmail-out/${F// /}"_2015.mbox"
   grepmail -d 2016 "$F" > ~/Desktop/grepmail-out/${F// /}"_2016.mbox"
-  grepmail -d 2010 "$F" > ~/Desktop/grepmail-out/${F// /}"_2017.mbox"
-  grepmail -d 2010 "$F" > ~/Desktop/grepmail-out/${F// /}"_2018.mbox"
-  grepmail -d 2010 "$F" > ~/Desktop/grepmail-out/${F// /}"_2019.mbox"
-  grepmail -d 2010 "$F" > ~/Desktop/grepmail-out/${F// /}"_2020.mbox"
+  grepmail -d 2017 "$F" > ~/Desktop/grepmail-out/${F// /}"_2017.mbox"
+  grepmail -d 2018 "$F" > ~/Desktop/grepmail-out/${F// /}"_2018.mbox"
+  grepmail -d 2019 "$F" > ~/Desktop/grepmail-out/${F// /}"_2019.mbox"
+  grepmail -d 2020 "$F" > ~/Desktop/grepmail-out/${F// /}"_2020.mbox"
 done
