774331f9a558fb923d1685156adb63c6d675e78e
Farrell <matthew.j.farrell@duke.edu>
2022-05-24 16:13:45 -0400

    moved files around for better organization

---
+++ bash/2022-update/batch_brunn-noclam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Very basic shell script to run brunnhilde.py over every subdirectory in a directory, without recursing.
+
+#unset a i
+#while IFS= read -r -u3 -d $'\0' d; do
+#    brunnhilde.py --hash sha1 -l ./objects/$d ~/Desktop/brunn-out "$d"
+#done 3< <(find $@ -maxdepth 1 -type f -print0)
+
+for D in *; do
+  brunnhilde.py -n "$D" ~/Desktop/brunn-out/ ${D// /}"_brunnout"
+done
