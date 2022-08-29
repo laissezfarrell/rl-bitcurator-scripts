ae03a502a2702a2d5ed43738a82c8fa5ee66ad6b
Farrell <matthew.j.farrell@duke.edu>
2022-08-29 12:44:35 -0400

    updated basic

---
+++ bash/2022-update/prompt-work/brunn-logical-basic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Very basic shell script to prompt a user for a starting directory, and then run brunnhilde.py over every immediat child directory, without recursing.
+
+echo What is the directory from which to start?
+read STARTDIR
+echo Where should the reports be saved?
+read OUTDIR
+echo Starting a batch operation at $STARTDIR and saving reports to $OUTDIR
+for D in $STARTDIR; 
+  echo $D
+  #brunnhilde.py --hash sha1 -l "$D" $OUTDIR/${D// /}"_brunnout"
+done
