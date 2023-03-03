04775587769b8180fcff5dde52e6281c4d19e882
Farrell <matthew.j.farrell@duke.edu>
2023-03-03 10:33:00 -0500

    modified ft-query.sh slightly for screencasting

--- bash/ft-query.sh
+++ bash/ft-query.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-export PATH=$PATH:/c/Users/mjf33/AppData/Local/Programs/Git/mingw64/bin #will need to edit this line if using on different Windows machine. Can probably comment this line if running on Linux/Mac.
+#export PATH=$PATH:/c/Users/mjf33/AppData/Local/Programs/Git/mingw64/bin #will need to edit this line if using on different Windows machine. Can probably comment this line if running on Linux/Mac.
 echo What is your token?
 read TOKEN
-# TOKEN=[your token here] -- note: if you'd rather not copy/paste your token every time you run the script, paste it here, and uncomment the line. If you do that, a) comment out lines 3 and 5, and b) don't share the script file publicly 
+# TOKEN=[your token here] #-- note: if you'd rather not copy/paste your token every time you run the script, paste it here, and uncomment the line. If you do that, a) comment out lines 3 and 5, and b) don't share the script file publicly 
 echo What is the File Tracker path?
 read ftPATH
 URLPATH=$(echo $ftPATH | sed 's/\//%2F/g')
